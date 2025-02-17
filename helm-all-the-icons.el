35e9be8ed7df736da07994ec4df8cfa650d61532
Thierry Volpiatto <thievol@posteo.net>
2025-02-17 07:08:48 +0100

    Add support for nerd-icons

--- helm-all-the-icons.el
+++ helm-all-the-icons.el
@@ -1,4 +1,4 @@
-;;; helm-all-the-icons.el --- Browse icons from all-the-icons package. -*- lexical-binding: t -*-
+;;; helm-all-the-icons.el --- Browse icons from icons packages. -*- lexical-binding: t -*-
 
 ;; Author:      Thierry Volpiatto <thievol@posteo.net>
 ;; Copyright (C) 2021 Thierry Volpiatto <thievol@posteo.net>
@@ -22,29 +22,35 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; Commentary: Provide support for both all-the-icons and nerd-icons.
+
 ;;; Code:
 
 (require 'cl-lib)
 (require 'helm)
-(require 'all-the-icons)
 
-(defvar helm-all-the-icons-alist (mapcar (lambda (family)
-                                           `(,family
-                                             ,(intern-soft (format "all-the-icons-%s-data" family))
-                                             ,(intern-soft (format "all-the-icons-%s" family))))
-                                         all-the-icons-font-families))
+(defvar nerd-icons-glyph-sets)
+(defvar all-the-icons-font-families)
+
+(defvar helm-all-the-icons-alist nil)
+(defvar helm-nerd-icons-alist nil)
 
 (defvar helm-all-the-icons--cache (make-hash-table))
-(defun helm-all-the-icons-build-source (family dfn ifn &optional reporter)
+(defvar helm-nerd-icons--cache (make-hash-table))
+
+(defun helm-all-the-icons-build-source (provider family dfn ifn &optional reporter)
   "Build source for FAMILY using data fn DFN and insert fn IFN.
 DFN is all-the-icons-<FAMILY>-data and IFN is all-the-icons-<FAMILY>
 function."
   (let* ((data    (funcall dfn))
          (max-len (cl-loop for (s . _i) in data
-                           maximize (length s))))
+                           maximize (length s)))
+         (cache (helm-acase provider
+                  (all-the-icons helm-all-the-icons--cache)
+                  (nerd-icons helm-nerd-icons--cache))))
     (helm-build-sync-source (symbol-name family)
       :init (lambda ()
-              (unless (gethash family helm-all-the-icons--cache)
+              (unless (gethash family cache)
                 (puthash family
                          (cl-loop for (name . icon) in data
                                   for fmt-icon = (funcall ifn name)
@@ -54,8 +60,8 @@ function."
                                                          (1+ (- max-len (length name))) ? )
                                                         (format "%s" fmt-icon))
                                                 (cons name icon)))
-                         helm-all-the-icons--cache)))
-      :candidates (lambda () (gethash family helm-all-the-icons--cache))
+                         cache)))
+      :candidates (lambda () (gethash family cache))
       :action `(("insert icon" . (lambda (candidate)
                                    (let ((fmt-icon (funcall ',ifn (car candidate))))
                                      (insert (format "%s" fmt-icon)))))
@@ -75,18 +81,44 @@ function."
      (progn
        ,@body)))
 
-(defun helm-all-the-icons-sources ()
+(defun helm-all-the-icons-sources (provider)
   (cl-declare (special reporter))
   (helm-all-the-icons-with-progress
-   (cl-loop for (family dfn fn) in helm-all-the-icons-alist
-            collect (helm-all-the-icons-build-source family dfn fn reporter))))
+   (cl-loop with alist = (helm-acase provider
+                           (all-the-icons helm-all-the-icons-alist)
+                           (nerd-icons helm-nerd-icons-alist))
+            for (family dfn fn) in alist
+            collect (helm-all-the-icons-build-source
+                     provider family dfn fn reporter))))
+
+;;;###autoload
+(defun helm-nerd-icons (&optional refresh)
+  (interactive "P")
+  (require 'nerd-icons)
+  (unless helm-nerd-icons-alist
+    (setq helm-nerd-icons-alist
+          (mapcar (lambda (family)
+                    `(,family
+                      ,(intern-soft (format "nerd-icons-%s-data" family))
+                      ,(intern-soft (format "nerd-icons-%s" family))))
+                  nerd-icons-glyph-sets)))
+  (when refresh (clrhash helm-nerd-icons--cache))
+  (helm :sources (helm-all-the-icons-sources 'nerd-icons)
+        :buffer "*helm all the icons*"))
 
 ;;;###autoload
 (defun helm-all-the-icons (&optional refresh)
   (interactive "P")
   (require 'all-the-icons)
+  (unless helm-all-the-icons-alist
+    (setq helm-all-the-icons-alist
+          (mapcar (lambda (family)
+                    `(,family
+                      ,(intern-soft (format "all-the-icons-%s-data" family))
+                      ,(intern-soft (format "all-the-icons-%s" family))))
+                  all-the-icons-font-families)))
   (when refresh (clrhash helm-all-the-icons--cache))
-  (helm :sources (helm-all-the-icons-sources)
+  (helm :sources (helm-all-the-icons-sources 'all-the-icons)
         :buffer "*helm all the icons*"))
 
 (provide 'helm-all-the-icons)
